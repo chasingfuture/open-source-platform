c9166cff5fe0ab7dc39125cae4229f710f0c7707
Simon Ninon <simon.ninon@gmail.com>
2017-11-21 23:53:26 -0800

    slim & root routing

---
+++ app/controllers/root_controller.rb
@@ -0,0 +1,8 @@
+class RootController < ApplicationController
+
+  # GET /
+  # Root Path
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # root routing
+  # use a dedicated controller for this specific route
+  root to: 'root#index'
+
 end
