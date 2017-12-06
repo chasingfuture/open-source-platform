a56cf56c16a052d4606af821340a7600fbb919c3
Simon Ninon <simon.ninon@gmail.com>
2017-12-05 23:33:47 -0800

    project model specs

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -9,4 +9,13 @@ FactoryBot.define do
     homepage_url    "https://cylix.github.io/cpp_redis"
   end
 
+  factory :project_tacopie, class: Project do
+    slug            "tacopie"
+    name            "tacopie"
+    description     "C++11 TCP client & server"
+    repository_url  "https://github.com/cylix/tacopie"
+    git_url         "git@github.com:Cylix/tacopie.git"
+    homepage_url    "https://cylix.github.io/tacopie"
+  end
+
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,165 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+
+    let(:project) { FactoryBot::build :project_cpp_redis }
+
+    describe 'default project' do
+
+      it 'should be valid' do
+        expect(project.valid?).to be_truthy
+      end
+
+    end
+
+    describe 'name' do
+
+      describe 'nil' do
+
+        before { project.name = nil }
+
+        it 'should not be valid' do
+          expect(project.valid?).to be_falsey
+        end
+
+      end
+
+      describe 'empty' do
+
+        before { project.name = "" }
+
+        it 'should not be valid' do
+          expect(project.valid?).to be_falsey
+        end
+
+      end
+
+      describe 'blank' do
+
+        before { project.name = "   " }
+
+        it 'should not be valid' do
+          expect(project.valid?).to be_falsey
+        end
+
+      end
+
+    end
+
+    describe 'repository_url' do
+
+      describe 'nil' do
+
+        before { project.repository_url = nil }
+
+        it 'should not be valid' do
+          expect(project.valid?).to be_falsey
+        end
+
+      end
+
+      describe 'empty' do
+
+        before { project.repository_url = "" }
+
+        it 'should not be valid' do
+          expect(project.valid?).to be_falsey
+        end
+
+      end
+
+      describe 'blank' do
+
+        before { project.repository_url = "   " }
+
+        it 'should not be valid' do
+          expect(project.valid?).to be_falsey
+        end
+
+      end
+
+    end
+
+    describe 'git_url' do
+
+      describe 'nil' do
+
+        before { project.git_url = nil }
+
+        it 'should not be valid' do
+          expect(project.valid?).to be_falsey
+        end
+
+      end
+
+      describe 'empty' do
+
+        before { project.git_url = "" }
+
+        it 'should not be valid' do
+          expect(project.valid?).to be_falsey
+        end
+
+      end
+
+      describe 'blank' do
+
+        before { project.git_url = "   " }
+
+        it 'should not be valid' do
+          expect(project.valid?).to be_falsey
+        end
+
+      end
+
+    end
+
+    describe 'slug' do
+
+      describe 'nil' do
+
+        before { project.slug = nil }
+
+        it 'should not be valid' do
+          expect(project.valid?).to be_falsey
+        end
+
+      end
+
+      describe 'empty' do
+
+        before { project.slug = "" }
+
+        it 'should not be valid' do
+          expect(project.valid?).to be_falsey
+        end
+
+      end
+
+      describe 'blank' do
+
+        before { project.slug = "   " }
+
+        it 'should not be valid' do
+          expect(project.valid?).to be_falsey
+        end
+
+      end
+
+      describe 'not unique' do
+
+        before { FactoryBot::create :project_tacopie, slug: project.slug }
+
+        it 'should not be valid' do
+          expect(project.valid?).to be_falsey
+        end
+
+      end
+
+    end
+
+  end
+
 end
