d6e677683c5aef362d3cd550778ba7614d3f5af0
Simon Ninon <simon.ninon@gmail.com>
2017-12-09 17:04:43 -0800

    user model specs

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+
+  factory :user_simon_ninon, class: User do
+    name            "Simon Ninon"
+    email           "simon.ninon@gmail.com"
+    github_ext_id   "100"
+  end
+
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,131 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe 'validations' do
+
+    let(:user) { FactoryBot::build :user_simon_ninon }
+
+    describe 'default user' do
+
+      it 'should be valid' do
+        expect(user.valid?).to be_truthy
+      end
+
+    end
+
+    describe 'name' do
+
+      describe 'nil' do
+
+        before { user.name = nil }
+
+        it 'should not be valid' do
+          expect(user.valid?).to be_falsey
+        end
+
+      end
+
+      describe 'empty' do
+
+        before { user.name = "" }
+
+        it 'should not be valid' do
+          expect(user.valid?).to be_falsey
+        end
+
+      end
+
+      describe 'blank' do
+
+        before { user.name = "   " }
+
+        it 'should not be valid' do
+          expect(user.valid?).to be_falsey
+        end
+
+      end
+
+    end
+
+    describe 'email' do
+
+      describe 'nil' do
+
+        before { user.email = nil }
+
+        it 'should not be valid' do
+          expect(user.valid?).to be_falsey
+        end
+
+      end
+
+      describe 'empty' do
+
+        before { user.email = "" }
+
+        it 'should not be valid' do
+          expect(user.valid?).to be_falsey
+        end
+
+      end
+
+      describe 'blank' do
+
+        before { user.email = "   " }
+
+        it 'should not be valid' do
+          expect(user.valid?).to be_falsey
+        end
+
+      end
+
+    end
+
+    describe 'github_ext_id' do
+
+      describe 'nil' do
+
+        before { user.github_ext_id = nil }
+
+        it 'should not be valid' do
+          expect(user.valid?).to be_falsey
+        end
+
+      end
+
+      describe 'empty' do
+
+        before { user.github_ext_id = "" }
+
+        it 'should not be valid' do
+          expect(user.valid?).to be_falsey
+        end
+
+      end
+
+      describe 'blank' do
+
+        before { user.github_ext_id = "   " }
+
+        it 'should not be valid' do
+          expect(user.valid?).to be_falsey
+        end
+
+      end
+
+      describe 'not unique' do
+
+        before { FactoryBot::create :user_simon_ninon, github_ext_id: user.github_ext_id }
+
+        it 'should not be valid' do
+          expect(user.valid?).to be_falsey
+        end
+
+      end
+
+    end
+
+  end
+
+end
